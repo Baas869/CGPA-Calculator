bd9c6b2126b8e1028f1397eeb3c940b22fd3c0a5
baas869 <sulaimanboladale869@gmail.om>
2025-02-18 14:24:16 +0100

    hook

--- src/components/PaymentStatus.jsx
+++ src/components/PaymentStatus.jsx
@@ -29,7 +29,7 @@ const PaymentStatus = () => {
         console.log("Verifying Payment with reference:", transactionReference);
 
         const response = await axios.post(
-          "https://your-backend-domain.com/payment/webhook/",
+          "https://cgpacalculator-0ani.onrender.com/payment/payment/webhook/",
           { reference: transactionReference }, // ✅ Send the correct reference
           { headers: { "Content-Type": "application/json" } }
         );
